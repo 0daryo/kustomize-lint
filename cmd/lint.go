0d2902732c5c928dec2524643dff128250893fa4
ryotaro oda <plm23.ccc@gmail.com>
2019-12-08 00:14:44 +0900

    feat: description

--- cmd/lint.go
+++ cmd/lint.go
@@ -23,13 +23,8 @@ import (
 // lintCmd represents the lint command
 var lintCmd = &cobra.Command{
 	Use:   "lint",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "check newTag in production and staging dir ",
+	Long:  `check newTag in production and staging dir`,
 	Run: func(cmd *cobra.Command, args []string) {
 		src.Lint()
 	},
